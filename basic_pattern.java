10a7200cbde9204bbbe3349b75ed46ea2231602e
Neha Makru <darkmelody04@gmail.com>
2025-07-07 18:23:53 +0530

    Update basic_pattern.java

--- basic_pattern.java
+++ basic_pattern.java
@@ -2,15 +2,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
     int rows, col;
-    String exp;
+    char exp;
     Scanner sc=new Scanner(System.in);
     System.out.println("Enter the number of rows you wish to have: ");
     rows=sc.nextInt();
     System.out.println(("Enter the number of columns you wish to have: "));
     col=sc.nextInt();
     System.out.println("Enter the character you wish to fill the matrix with: ");
-    sc.nextLine();
-    exp=sc.nextLine();
+    exp=sc.next().charAt(0); 
     for (int i=1;i<=rows;i++){
         for (int j=1;j<=col;j++){
             System.out.print(exp);
